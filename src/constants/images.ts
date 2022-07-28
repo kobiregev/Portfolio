7a56ee8d484b6c5bea45bfec54956bc4c62e7f05
kobiregev <regev79@gmail.com>
2022-07-28 19:01:01 +0300

    Updates

--- src/constants/images.ts
+++
@@ -1,5 +0,0 @@
-import profile from "../assets/profile.jpg";
-
-export default {
-  profile,
-};
--- src/constants/links.ts
+++
@@ -1,5 +0,0 @@
-export enum Links {
-  LINKEDIN = "https://www.linkedin.com/in/kobi-regev-94a2381b8/",
-  GITHUB = "https://github.com/kobiregev",
-  YOUTUBE = "https://www.youtube.com/channel/UCrgcktOG-BhjtVp6EegpZaA",
-}
